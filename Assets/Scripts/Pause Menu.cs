0108b141d8ec8c95f9e87f3f15522d1445c7fc75
Alex Pham <67773092+apalex@users.noreply.github.com>
2025-02-13 20:30:41 -0500

    Pause Menu Works

--- Assets/Scripts/Pause Menu.cs
+++ Assets/Scripts/Pause Menu.cs
@@ -7,23 +7,41 @@ public class Pause : MonoBehaviour
 {
 
     [SerializeField] GameObject pauseMenu;
+    public static bool isPaused;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (Time.timeScale == 0)
+            if (isPaused)
             {
-                pauseMenu.SetActive(true);
-                Time.timeScale = 1;
-
+                UnPauseGame();
             } else
             {
-                pauseMenu.SetActive(false);
-                Time.timeScale = 0;
+                PauseGame();
             }
         }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void UnPauseGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
     }
 
     void Restart()
