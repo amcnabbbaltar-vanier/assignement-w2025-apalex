9755ad4ce0aee5b74b5321131d4ff4f31f777799
Alex Pham <67773092+apalex@users.noreply.github.com>
2025-02-15 18:40:17 -0500

    Timer Done, Score HUD done

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Component")]
+    public TextMeshProUGUI timerText;
+
+    [Header("Timer Settings")]
+    public float currentTime;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = currentTime + Time.deltaTime;
+        timerText.text = "Time: " + currentTime.ToString("0.00");
+    }
+}
